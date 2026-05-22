1225b7e668a89b2e832eda60553bbaeeee29001b
Diya <diyamaru32@gmail.com>
2026-05-22 17:11:59 +0530

    Create Class1.java

---
+++ JAVA/Lab11/diya/p1/Class1.java
@@ -0,0 +1,8 @@
+package p1;
+public class Class1
+{
+	public String publicVar = "I am public";
+	private String privateVar = "I am private";
+	protected String protectedVar = "I am protected";
+	String defaultVar = "I am default"; 
+}
