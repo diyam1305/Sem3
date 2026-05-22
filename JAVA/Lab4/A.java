078603ae42e1c0ebc01135ff413c8248919bc144
Diya <diyamaru32@gmail.com>
2026-05-22 16:40:24 +0530

    Create A.java

---
+++ JAVA/Lab4/A.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class A
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter size of array:");
+		int n = sc.nextInt();
+		int []arr = new int[n];
+		for(int i=0; i<n; i++)
+		{
+			System.out.print("Enter" + i + "th element:");
+			arr[i] = sc.nextInt();
+		}
+		for(int i=0; i<n; i++)
+		{
+			System.out.print(arr[i]);
+		}
+	}
+}
