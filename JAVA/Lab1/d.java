69fcc3c79568cf7afc7b51fb57c3aac7f4272baa
Diya <diyamaru32@gmail.com>
2026-05-22 16:37:35 +0530

    Add files via upload

---
+++ JAVA/Lab1/d.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class d
+	{
+		public static void main(String[] args)
+		{
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter value of a and b:");
+			int a=sc.nextInt();
+			int b=sc.nextInt();
+			int ans=a+b;
+			System.out.println("Total is="+ans);
+		}
+	}
