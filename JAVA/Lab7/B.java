74e58644784790a52f1d4f9f31a13ebf828fa929
Diya <diyamaru32@gmail.com>
2026-05-22 16:48:04 +0530

    Add files via upload

---
+++ JAVA/Lab7/B.java
@@ -0,0 +1,16 @@
+public class B
+{
+	public static void main(String[]args)
+	{
+		int a=10, b=20;
+		System.out.println(Math.max(a,b));
+		System.out.println(Math.min(a,b));
+		System.out.println(Math.random());
+		System.out.println(Math.pow(2,4));
+		System.out.println(Math.sqrt(16));
+		System.out.println(Math.round(5.6));
+		System.out.println(Math.ceil(5.1));
+		System.out.println(Math.floor(5.9));
+		System.out.println(Math.abs(-1));
+	}
+}
---
+++ JAVA/Lab7/C.java
@@ -0,0 +1,16 @@
+public class C
+{
+	public static void main(String[] args)
+	{
+		int a=10, b=20;
+		System.out.println("Maximum number is:" + Math.max(a,b));
+		System.out.println("Minimum number is:" + Math.min(a,b));
+		System.out.println("Random number is:" + Math.random());
+		System.out.println("Power of number is:" + Math.pow(2,4));
+		System.out.println("Square root of number is:" + Math.sqrt(16));
+		System.out.println("Rounded number is:" + Math.round(5.6));
+		System.out.println("Ceiled number is:" + Math.ceil(5.1));
+		System.out.println("Floored number is:" + Math.floor(5.9));
+		System.out.println("Absolute number is:" + Math.abs(-1));
+	}
+}
